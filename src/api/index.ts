f24047425779962e209cffe91299f77b3113f9ec
길상혁 <blain1@naver.com>
2020-07-03 14:12:49 +0900

    api 수정

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: `https://api.mypetmily.net/api`,
+  baseURL: `https://api.mypetmily.net`,
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
